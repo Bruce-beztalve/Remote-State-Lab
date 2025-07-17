2030782cfdd0fa07d2f7ca4eeb15195daf4aaea7
Bruce Banner <bruce@beztalve.com>
2025-07-17 16:42:25 +1000

    adding infrastructure

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = "~> 1.12.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.43.0"
+    }
+  }
+  cloud {
+    organization = "20221110-Test-Org"
+    workspaces {
+      name = "TerraformCI"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-test1"
+  location = "Australia Southeast"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "shaklsdy82os"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
