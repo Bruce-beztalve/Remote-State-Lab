7dbf0d50a6ed5df0aa1792d15e0956a0bb9e8faf
Bruce Banner <bruce@beztalve.com>
2025-07-17 16:57:29 +1000

    Imported extings and added new storage

--- main.tf
+++ main.tf
@@ -31,3 +31,11 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_account" "storage2" {
+  name                     = "shasyi88s7ud"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
